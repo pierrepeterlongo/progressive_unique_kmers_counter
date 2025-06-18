8821e0254ee2cc5855e56092c26c2f2c06c959e9
Pierre Peterlongo <pierre.peterlongo@inria.fr>
2025-06-18 12:13:39 +0200

    Plot the Estimated growth trend

--- plot_progression.py
+++ plot_progression.py
@@ -19,7 +19,7 @@ def main(input_file_path):
             nb_lines += 1
             if line.startswith('Processed'):
                 nb_lines += 1
-                if nb_lines % 1 == 0:
+                if nb_lines % 10 == 0:
                     parts = line.split()
                     Nb_kmers_seen = int(parts[3])
                     nb_unique_kmers = int(parts[6].rstrip(','))
@@ -28,18 +28,24 @@ def main(input_file_path):
                     last_nb_unique_kmers = nb_unique_kmers
             if line.startswith('Estimated total kmers'):
                 parts = line.strip().split()
-                total_estimated_kmers = int(parts[-1])
+                if parts[-1].isdigit():
+                    # line: Estimated total kmers: 13994639
+                    # get the total estimated kmers
+                    total_estimated_kmers = int(parts[-1])
             # line: Stable curve found 10/10: f(kmers) = ? + 0.10786222*kmers + -0.000055000186*kmers^2
             # get the growth from the "*kmer" factor, here 0.10786222
             if line.startswith('Stable curve found'):
-                parts = line.strip().split()
-                growth = float(parts[6].split('*')[0])
+                part = line.strip().split()[-3]
+                growth = float(part.split('*')[0])
              
 
     # Get the file name without extension for the plot title
     file_name = input_file_path.split('/')[-1].split('.')[0]
     # Plotting to f'{file_name}_kmers_plot.png'
     plt.figure(figsize=(10, 6))
+    # plot log x values: 
+    # plt.xscale('log')
+    # plt.yscale('log')
     plt.plot(df['Nb kmers seen'], df['Nb unique k-mers'], marker='o')
     plt.title(f'{file_name} Nb distinct solid canonical k-mers vs Nb total of kmer')
     plt.xlabel('Number of kmers seen')
@@ -48,7 +54,8 @@ def main(input_file_path):
     
     # Plotting on the same graph the tendency line: with x from last_nb_kmers_seen to total_estimated_kmers
     # with the plot: f(x)= last_nb_kmers_seen + growth * (x - last_nb_kmers_seen)
-    x_values = range(last_nb_kmers_seen, total_estimated_kmers + 1, step=100000)
+    total_estimated_kmers = last_nb_kmers_seen + 10000000
+    x_values = range(last_nb_kmers_seen, total_estimated_kmers + 1, 100000)
     y_values = [last_nb_unique_kmers + growth * (x - last_nb_kmers_seen) for x in x_values]
     plt.plot(x_values, y_values, color='red', linestyle='--', label='Estimated growth trend')
     
